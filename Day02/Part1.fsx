032bbbbb4e12d55de24b2f2c5b5a210e6dd16649
chris <christoph7@gmail.com>
2022-12-03 17:11:49 +0100

    Format part one of day two

--- Day02/Part1.fsx
+++ Day02/Part1.fsx
@@ -5,19 +5,22 @@ type Shape =
     | Paper
     | Scissors
 
+type Outcome =
+    | Lose
+    | Draw
+    | Win
+
 type Round = { Opponent: Shape; Me: Shape }
 
-type Outcome = { OpponentScore: int; MyScore: int}
+type Score = { OpponentScore: int; MyScore: int }
 
-let parseFirstColumn =
-    function
+let parseFirstColumn = function
     | "A" -> Rock
     | "B" -> Paper
     | "C" -> Scissors
     | x -> failwithf "Unknown value '%s'" x
 
-let parseSecondColumn =
-    function
+let parseSecondColumn = function
     | "X" -> Rock
     | "Y" -> Paper
     | "Z" -> Scissors
@@ -31,27 +34,37 @@ let parse (line : string) =
         { Opponent = opponentShape; Me = myShape }
     | _ -> failwithf "Unexpected line '%s'" line
 
-let shapeScore =
-    function
+let shapeScore = function
     | Rock -> 1
     | Paper -> 2
     | Scissors -> 3
 
 let outcomeRound opponent me =
     match opponent, me with
-    | Rock, Rock -> 3
-    | Rock, Paper -> 6
-    | Rock, Scissors -> 0
-    | Paper, Rock -> 0
-    | Paper, Paper -> 3
-    | Paper, Scissors -> 6
-    | Scissors, Rock -> 6
-    | Scissors, Paper -> 0
-    | Scissors, Scissors -> 3
+    | Rock, Rock -> Draw
+    | Rock, Paper -> Win
+    | Rock, Scissors -> Lose
+    | Paper, Rock -> Lose
+    | Paper, Paper -> Draw
+    | Paper, Scissors -> Win
+    | Scissors, Rock -> Win
+    | Scissors, Paper -> Lose
+    | Scissors, Scissors -> Draw
+
+let outcomeScore = function
+    | Win -> 6
+    | Draw -> 3
+    | Lose -> 0
 
 let playRound round =
-    let opponentScore = outcomeRound round.Me round.Opponent + shapeScore round.Opponent
-    let myScore = outcomeRound round.Opponent round.Me + shapeScore round.Me
+    let opponentScore =
+        outcomeScore (outcomeRound round.Me round.Opponent)
+        + shapeScore round.Opponent
+
+    let myScore =
+        outcomeScore (outcomeRound round.Opponent round.Me)
+        + shapeScore round.Me
+        
     { OpponentScore = opponentScore; MyScore = myScore }
 
 let solve lines =
@@ -60,5 +73,4 @@ let solve lines =
     |> Seq.sumBy (fun outcome -> outcome.MyScore)
 
 let input = File.ReadLines (Path.Combine (__SOURCE_DIRECTORY__, "input.txt"))
-
 solve input
