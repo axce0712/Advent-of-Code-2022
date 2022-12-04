d1b1a3100e30b8dc3cac49dd32810ed1ac99715c
chris <christoph7@gmail.com>
2022-12-04 17:22:26 +0100

    Simplify solution for day four

--- Day04/Optimized.fsx
+++ Day04/Optimized.fsx
@@ -1,35 +1,32 @@
-open System
 open System.IO
 
 type Section = { Start: int; End: int }
 
-type Assignment = { FirstSection: Section; SecontSection: Section }
+type Assignment = { First: Section; Second: Section }
 
 let isSubset s1 s2 =
     s1.Start <= s2.Start && s1.End >= s2.End
 
+let containsFully s1 s2 =
+    isSubset s1 s2 || isSubset s2 s1
+
 let isOverlapping s1 s2 =
     s1.End >= s2.Start && s1.Start <= s2.End
     
-let (|Int|) (input: string) = Int32.Parse input
-
-let parse (line : string) =
-    match line.Split(",") |> Array.map (fun section -> section.Split("-")) with
-    | [| [| Int a; Int b |]
-         [| Int c; Int d |] |] ->
-        { FirstSection = { Start = a; End = b }
-          SecontSection = { Start = c; End = d } }
-    | _ ->
-        failwith $"Cannot parse line '%s{line}'"
+let parse (line : string) = // "3-78,73-77"
+    let [| [| s1; e1 |]; [| s2; e2 |] |] =
+        line.Split(",") // ["3-78";"73-77"]
+        |> Array.map (fun x -> x.Split("-")) // [["3";"78"];["73";"77"]]
+        
+    { First = { Start = int s1; End = int e1 }
+      Second = { Start = int s2; End = int e2 } }
 
 let solve f lines =
     lines
     |> Seq.map parse
-    |> Seq.filter (fun a -> f a.FirstSection a.SecontSection)
+    |> Seq.filter (fun { First = s1; Second = s2 } -> f s1 s2)
     |> Seq.length
 
-#time
 let input = File.ReadLines (Path.Combine (__SOURCE_DIRECTORY__, "input.txt"))
-printfn "%i" (solve (fun s1 s2 -> isSubset s1 s2 || isSubset s2 s1) input)
+printfn "%i" (solve containsFully input)
 printfn "%i" (solve isOverlapping input)
-#time
