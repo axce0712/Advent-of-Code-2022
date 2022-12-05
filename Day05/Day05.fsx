36f374e1b70dcd4b432702f2c7eddead02399443
chris <christoph7@gmail.com>
2022-12-05 22:54:20 +0100

    Simplify solution for day 5 with List.transpose

--- Day05/Day05.fsx
+++ Day05/Day05.fsx
@@ -1,75 +1,49 @@
 open System
 open System.IO
 
-type Move = { Count: int; StackSource: int; StackTarget: int }
+type Move = { Count: int; SourceStack: int; TargetStack: int }
+
+let parseStacks lines =
+    let extracted =
+        lines
+        |> Seq.map (fun line ->
+            Seq.indexed line
+            |> Seq.filter (fun (i, _) -> ((i - 1) % 4) = 0)
+            |> Seq.map snd
+            |> Seq.toList)
+
+    let stacks =
+        List.transpose extracted
+        |> List.map (fun xs ->
+            let (y :: ys) =
+                List.rev xs
+                |> List.filter (fun x -> x <> ' ')
+
+            (int (string y), List.rev ys))
+    
+    Map.ofList stacks
 
 let parseMove (line : string) =
     let words = line.Split(" ")
     let count = int words[1]
     let source = int words[3]
     let target = int words[5]
-    { Count = count; StackSource = source; StackTarget = target }
-
-let parseCrate stacks (line : string) =
-    stacks
-    |> List.choose (fun (i, n) ->
-        let candidate = line[i]
-        
-        if Char.IsLetter(candidate) then
-            Some(n, candidate)
-        else
-            None)
-
-let parseCrates stacks lines =
-    let cratePositions =
-        lines
-        |> Seq.rev
-        |> Seq.map (parseCrate stacks)
-
-    let crates =
-        (Map.empty, cratePositions)
-        ||> Seq.fold (fun acc ps ->
-            (acc, ps)
-            ||> Seq.fold (fun acc (n, c) ->
-                let newCrates =
-                    Map.tryFind n acc
-                    |> Option.map (fun cs -> c :: cs)
-                    |> Option.defaultValue [ c ]
-                    
-                Map.add n newCrates acc))
-            
-    crates
+    { Count = count; SourceStack = source; TargetStack = target }
 
 let parse newLine (input : string) =
     let parts = input.Split(String.replicate 2 newLine)
-    let cratesPartLines = parts[0].Split(newLine)
-    let stacks =
-        Array.last cratesPartLines
-        |> Seq.indexed
-        |> Seq.filter (snd >> Char.IsDigit)
-        |> Seq.map (fun (i, x) -> (i, int (string x)))
-        |> Seq.toList
-
-    let crates = parseCrates stacks (cratesPartLines[..^1])
-    let moves =
-        parts[1].Split(newLine)
-        |> Seq.map parseMove
-        |> Seq.toList
-
-    (crates, moves)
+    let stacks = parseStacks (parts[0].Split(newLine))
+    let moves = parts[1].Split(newLine) |> Seq.map parseMove |> Seq.toList
+    (stacks, moves)
 
 let move f stacks instruction =
-    let stackSource = Map.find instruction.StackSource stacks
-    let stackTarget = Map.find instruction.StackTarget stacks
-    let newStackTarget =
-        (stackSource |> List.take instruction.Count |> f)
-        @ stackTarget
-
-    let newStackSource = stackSource |> List.skip instruction.Count
+    let sourceStack = Map.find instruction.SourceStack stacks
+    let targetStack = Map.find instruction.TargetStack stacks
+    let taken, left = List.splitAt instruction.Count sourceStack
 
     stacks
-    |> Map.add instruction.StackSource newStackSource
-    |> Map.add instruction.StackTarget newStackTarget
+    |> Map.add instruction.SourceStack left
+    |> Map.add instruction.TargetStack ((f taken) @ targetStack)
 
 let solve f (stacks, instructions) =
     let finalStacks = List.fold (move f) stacks instructions
