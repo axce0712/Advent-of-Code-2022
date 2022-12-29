59fa99ed4c54e3ae6ed9feae7d256bd8d02e31e5
chris <christoph7@gmail.com>
2022-12-29 15:17:13 +0100

    [day20] Optimize solution of part 1

--- Day20/Part1.fsx
+++ Day20/Part1.fsx
@@ -1,35 +1,47 @@
-let swap arr sourceIndex targetIndex =
-    let item = Array.item sourceIndex arr
-    Array.set arr sourceIndex (Array.item targetIndex arr)
-    Array.set arr targetIndex item
+let indexMoves length moves =
+    if moves < 0 then
+        moves % length - 1
+    else if moves > 0 then
+        moves % (length - 1)
+    else
+        0
+let nextIndex length index moves =
+    let indexMoves = indexMoves length moves
+    let increment =
+        if index + indexMoves > length then 1 else 0
 
-let adjust indexMap length index =
-    (indexMap index + length) % length
+    (index + indexMoves + increment + length) % length
 
-let mix (encryptedFile : int array) : int array =
-    let indexes = Array.init encryptedFile.Length id
-    let lastIndex = encryptedFile.Length - 1
-    let mutable offset = 0
-    for index in 0 .. lastIndex do
-        let moves = Array.item index encryptedFile
-        let indexMap =
-            if moves > 0 then fun x -> x + 1
-            else if moves < 0 then fun x -> x - 1
-            else id
+nextIndex 7 0 1
+nextIndex 7 0 2
+nextIndex 7 1 -3
+nextIndex 7 2 3
+nextIndex 7 2 -2
+nextIndex 7 3 0
+nextIndex 7 5 4
 
-        for _ in 1 .. abs moves do
-            let actual = indexes[index]
-            let next = adjust indexMap encryptedFile.Length actual
-            let nextIndex = Array.findIndex ((=) next) indexes
-            swap indexes index nextIndex
-            offset <-
-                match actual, next with
-                | 0, idx when idx = lastIndex -> offset - 1
-                | idx, 0 when idx = lastIndex -> offset + 1
-                | _ -> offset
+let adjustIndex length (oldIndex, newIndex) index =
+    if index = oldIndex then
+        newIndex
+    else if newIndex > oldIndex && oldIndex < index && index <= newIndex then
+        (index - 1 + length) % length
+    else if oldIndex > newIndex && newIndex <= index && index < oldIndex then
+        (index + 1 + length) % length
+    else
+        index
 
-    encryptedFile
-    |> Array.permute (adjust (fun i -> indexes[i] + offset) encryptedFile.Length)
+let mix encrypted =
+    let indexes = Array.init (Array.length encrypted) id
+    for index, moves in Array.indexed encrypted do
+        let oldIndex = indexes[index]
+        let newIndex =  nextIndex encrypted.Length oldIndex moves
+        for i in 0 .. indexes.Length - 1 do
+            let currentIndex = indexes[i]
+            indexes[i] <- adjustIndex encrypted.Length (oldIndex, newIndex) currentIndex
+
+    Array.permute (fun i -> indexes[i]) encrypted
+
+mix [| 1; 2; -3; 3; -2; 0; 4 |]
 
 let solve lines =
     let encrypted = Array.map int lines
